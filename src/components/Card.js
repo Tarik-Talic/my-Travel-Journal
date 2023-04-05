756a58fd35d4d7898e465f63c36d0fed33db1ba4
Tarik-Talic <talic.tarik@gmail.com>
2023-04-05 03:45:21 +0200

    Card styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./styles/Card.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 export default function Card(props) {
   return (
     <div className="card">
-      <img src={props.item.imageUrl} />
-      <span className="card-location">
-        <p>{props.item.location}</p>
-        <a href={props.item.googleMapsUrl}>View on Google Maps</a>
-      </span>
-      <h1>{props.item.title}</h1>
-      <p className="card-duration">{props.item.startDate}</p>
-      <p className="card-description">{props.item.description}</p>
+      <img alt="Image" src={props.item.imageUrl} />
+      <div className="card__info">
+        <span className="card-location">
+          <FontAwesomeIcon icon={faLocationDot} />
+          <p>{props.item.location}</p>
+          <a href={props.item.googleMapsUrl}>View on Google Maps</a>
+        </span>
+        <h1>{props.item.title}</h1>
+        <p className="card-duration">
+          {props.item.startDate} - {props.item.endDate}
+        </p>
+        <p className="card-description">{props.item.description}</p>
+      </div>
     </div>
   );
 }

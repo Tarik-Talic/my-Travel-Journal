8442e552187855f0ac68df757f0b48c476b0e8f8
Tarik-Talic <talic.tarik@gmail.com>
2023-04-06 00:25:52 +0200

    Added more card data

--- src/data.js
+++ src/data.js
@@ -2,14 +2,73 @@ export default [
   {
     id: 1,
     title: "Mount Fuji",
-    location: "Japan",
+    location: "JAPAN",
     googleMapsUrl:
       "https://www.google.com/maps/place/Mount+Fuji/@35.360625,138.7273634,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA!5m1!1e4",
     startDate: "12 Jan, 2022",
     endDate: "25 Jan, 2022",
     description:
-      "Mount Fuji located on the island of Honshū, is the highest mountain in Japan, with a summit elevation of 3,776.24 m (12,389 ft 3 in). The mountain is located about 100 km (62 mi) southwest of Tokyo and is visible from there on clear days.",
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
     imageUrl:
-      "https://images.pexels.com/photos/3408353/pexels-photo-3408353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      "https://images.pexels.com/photos/3408354/pexels-photo-3408354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 2,
+    title: "Sydney Opera House",
+    location: "AUSTRALIA",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567841,151.2065634,15z/data=!3m2!4b1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m6!3m5!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0",
+    startDate: "27 May, 2021",
+    endDate: " 8 Jun, 2021",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+    imageUrl:
+      "https://images.pexels.com/photos/2845013/pexels-photo-2845013.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 3,
+    title: "Geirangerfjord",
+    location: "NORWAY",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Geirangerfjord/@62.1045668,6.7957276,10z/data=!3m1!4b1!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi",
+    startDate: "01 Oct, 2021",
+    endDate: " 18 Nov, 2021",
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl:
+      "https://images.pexels.com/photos/14037938/pexels-photo-14037938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 4,
+    title: "Mount Rushmore National Memorial",
+    location: "USA",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Mount+Rushmore+National+Memorial/@43.8803354,-103.4625079,15z/data=!3m1!4b1!4m6!3m5!1s0x877d35d8b53ed6df:0xdaf53dbe055cc641!8m2!3d43.8803357!4d-103.4537746!16zL20vMDE5ZHZ2",
+    startDate: "29 Nov, 2021",
+    endDate: "5 Dec, 2021",
+    description:
+      "The Mount Rushmore National Memorial is a national memorial centered on a colossal sculpture carved into the granite face of Mount Rushmore ( Six Grandfathers) in the Black Hills near Keystone, South Dakota, United States.",
+    imageUrl:
+      " https://images.pexels.com/photos/4134225/pexels-photo-4134225.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 5,
+    title: "",
+    location: "",
+    googleMapsUrl: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+    imageUrl: "",
+  },
+  {
+    id: 6,
+    title: "",
+    location: "",
+    googleMapsUrl: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+    imageUrl: "",
   },
 ];
